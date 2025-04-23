971870140cc5e3467aac92c842671b2cd770907e
Elba López Santos <slable@hotmail.com>
2025-04-23 12:59:51 +0200

    Autenticación, parte página publi product y añado categorias en initDB

--- FRONTED_compra_venta_tech/src/App.jsx
+++ FRONTED_compra_venta_tech/src/App.jsx
@@ -1,24 +1,32 @@
 // Importamos los Componentes
 import { Route, Routes } from "react-router-dom";
 import Header from "./components/Header/Header";
-import Menu from "./components/Menu/menu";  // Importa el componente Menu
-
+import Menu from "./components/Menu/menu"; // Importa el componente Menu
 
 // Importamos las páginas
 import Home from "./pages/Home";
-
+//import Login from "./pages/Login";
+//import NotFound from "./pages/NotFound";
+//import ProductDetail from "./pages/ProductDetail";
+import PublishProduct from "./pages/PublishProduct";
+//import Register from "./pages/Register";
+//import RequestList from "./pages/RequestList";
+//import UserDataAndChangePass from "./pages/UserDataAndChangePass";
+//import UserList from "./pages/UserList";
+//import UserProfile from "./pages/UserProfile";
+//import UserValidation from "./pages/UserValidation";
 
 function App() {
   return (
     <>
       <Header />
-      <Menu />  {/* Añadimos el componente Menu aquí */}
+      <Menu /> {/* Añadimos el componente Menu aquí */}
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/publicar" element={<PublishProduct />} />
       </Routes>
     </>
   );
 }
 
 export default App;
-
