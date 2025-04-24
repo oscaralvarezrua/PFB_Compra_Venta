5cc5a4fed12672d012c1531adbf50494c310f9f3
ZoeRuiz <zoe_alge89@outlook.es>
2025-04-24 19:04:24 +0200

    botones header funcionales, registro/login y vender

---
+++ FRONTED_compra_venta_tech/src/pages/Register.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Register = () => {
+  return (
+    <div style={{ padding: "2rem" }}>
+      <h2>Página de Registro</h2>
+      <p>En construcción</p>
+    </div>
+  );
+};
+
+export default Register;
