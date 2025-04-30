3459d584f2bd3e6a5e07a7af8a0d2cc373511878
Oscar Alvarez <oscaralvarez4c@gmail.com>
2025-04-30 18:18:17 +0200

    paginas de registro, modificacion de perfil y cambio de pass

--- FRONTED_compra_venta_tech/src/App.jsx
+++ FRONTED_compra_venta_tech/src/App.jsx
@@ -13,20 +13,19 @@ import Login from "./pages/Login";
 import UserDataAndChangePass from "./pages/UserDataAndChangePass";
 import NotFound from "./pages/NotFound";
 import UserProfile from "./pages/UserProfile";
+import UserValidation from "./pages/UserValidation";
 
 function App() {
   const location = useLocation(); // Obtener la ubicación actual de la ruta
 
   return (
     <>
-      {location.pathname !== "/register" &&
-        location.pathname !== "/login" &&
-        location.pathname !== "/changepassword" && (
-          <>
-            <Header />
-            <Menu />
-          </>
-        )}
+      {location.pathname !== "/register" && location.pathname !== "/login" && location.pathname !== "/changepassword" && (
+        <>
+          <Header />
+          <Menu />
+        </>
+      )}
 
       <Routes>
         <Route path="/" element={<Home />} />
@@ -35,9 +34,10 @@ function App() {
         <Route path="/filtrados" element={<SearchFilteredProducts />} />
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/user" element={<UserProfile />} />
+        <Route path="/user" element={<UserDataAndChangePass />} />
         <Route path="/user/notification" element={<UserProfile />} />
         <Route path="/changepassword" element={<UserDataAndChangePass />} />
+        <Route path="/validate/:validationCode" element={<UserValidation />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </>
